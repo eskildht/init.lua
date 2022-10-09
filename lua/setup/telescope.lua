1c7c36fe621c200cc781647f57b5a85b5177c75e
Eskild Hein Trøen <eskild.troen@bouvet.no>
2022-10-09 19:15:18 +0200

    add telescope commit

--- lua/setup/telescope.lua
+++ lua/setup/telescope.lua
@@ -1,7 +1,8 @@
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>s', ':Telescope find_files<cr>', opts)
-vim.keymap.set('n', '<leader>g', ':Telescope live_grep<cr>', opts)
-vim.keymap.set('n', '<leader>b', ':Telescope buffers<cr>', opts)
+vim.keymap.set('n', '<leader>s', ':lua require("telescope.builtin").find_files()<cr>', opts)
+vim.keymap.set('n', '<leader>g', ':lua require("telescope.builtin").live_grep()<cr>', opts)
+vim.keymap.set('n', '<leader>b', ':lua require("telescope.builtin").buffers()<cr>', opts)
+vim.keymap.set('n', '<leader>c', ':lua require("telescope.builtin").git_commits()<cr>', opts)
 
 require('telescope').setup({
   defaults = {
