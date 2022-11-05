fb70094705475df9b749e2e295e2dc6ae36c6dd5
Eskild Hein Trøen <eskild.troen@bouvet.no>
2022-11-05 12:36:24 +0100

    change live_grep_resume -> live_grep

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -1,7 +1,7 @@
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 keymap('n', '<leader>s', ':FzfLua files<cr>', opts)
-keymap('n', '<leader>/', ':FzfLua live_grep_resume<cr>', opts)
+keymap('n', '<leader>/', ':FzfLua live_grep<cr>', opts)
 keymap('n', '<leader>lc', ':FzfLua git_commits<cr>', opts)
 keymap('n', '<leader>ls', ':FzfLua git_status<cr>', opts)
 keymap('v', '<leader>/', '<esc>:FzfLua grep_visual<cr>', opts)
