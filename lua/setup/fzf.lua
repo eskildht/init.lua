5490042a5ce9e91e1dd4732e8c95ab95e3f1293f
Eskild Hein Trøen <eskild.troen@bouvet.no>
2022-11-06 12:07:56 +0100

    add buffers keymap

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -2,6 +2,7 @@ local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 keymap('n', '<leader>s', ':FzfLua files<cr>', opts)
 keymap('n', '<leader>/', ':FzfLua live_grep<cr>', opts)
+keymap('n', '<leader>b', ':FzfLua buffers<cr>', opts)
 keymap('n', '<leader>lc', ':FzfLua git_commits<cr>', opts)
 keymap('n', '<leader>ls', ':FzfLua git_status<cr>', opts)
 keymap('v', '<leader>/', '<esc>:FzfLua grep_visual<cr>', opts)
