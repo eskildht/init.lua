4e38ce8f397df4ee18b5b98e0c62630fb036eb96
Eskild Hein Trøen <eskild.troen@bouvet.no>
2022-11-06 14:20:07 +0100

    shorten keymaps

--- lua/setup/diffview.lua
+++ lua/setup/diffview.lua
@@ -1,5 +1,5 @@
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>do', ':DiffviewOpen<cr>', opts)
 vim.keymap.set('n', '<leader>dc', ':DiffviewClose<cr>', opts)
-vim.keymap.set('n', '<leader>dfh', ':DiffviewFileHistory %<cr>', opts)
-vim.keymap.set('n', '<leader>dbh', ':DiffviewFileHistory<cr>', opts)
+vim.keymap.set('n', '<leader>df', ':DiffviewFileHistory %<cr>', opts)
+vim.keymap.set('n', '<leader>db', ':DiffviewFileHistory<cr>', opts)
