0e392e84523bd9302e50c95be06fd857422668f6
Eskild Hein Trøen <eht@equinor.com>
2023-01-09 07:47:49 +0100

    add fzf bindings

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -1,8 +1,17 @@
+require('fzf-lua').setup({
+  keymap = {
+    fzf = {
+      ['ctrl-q'] = 'select-all+accept'
+    }
+  }
+})
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
-keymap('n', '<leader>s', ':FzfLua files<cr>', opts)
-keymap('n', '<leader>/', ':FzfLua live_grep<cr>', opts)
-keymap('n', '<leader>b', ':FzfLua buffers<cr>', opts)
-keymap('n', '<leader>lc', ':FzfLua git_commits<cr>', opts)
-keymap('n', '<leader>ls', ':FzfLua git_status<cr>', opts)
-keymap('v', '<leader>/', '<esc>:FzfLua grep_visual<cr>', opts)
+keymap('n', '<leader>sf', ':FzfLua files<cr>', opts)
+keymap('n', '<leader>sg', ':FzfLua live_grep<cr>', opts)
+keymap('n', '<leader>/', ':FzfLua lgrep_curbuf<cr>', opts)
+keymap('n', '<leader>sb', ':FzfLua buffers<cr>', opts)
+keymap('n', '<leader>sc', ':FzfLua git_commits<cr>', opts)
+keymap('n', '<leader>ss', ':FzfLua git_status<cr>', opts)
+keymap('n', '<leader>sk', ':FzfLua keymaps<cr>', opts)
+keymap('v', '<leader>sg', '<esc>:FzfLua grep_visual<cr>', opts)
