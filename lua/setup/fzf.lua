14f86d2c8ffe9ca2758f784917ea5128270cf1ec
Eskild Hein Trøen <eht@equinor.com>
2023-02-17 14:39:29 +0100

    change live_grep to live_grep_glob

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -8,7 +8,7 @@ require('fzf-lua').setup({
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 keymap('n', '<leader>sf', ':FzfLua files<cr>', opts)
-keymap('n', '<leader>sg', ':FzfLua live_grep<cr>', opts)
+keymap('n', '<leader>sg', ':FzfLua live_grep_glob<cr>', opts)
 keymap('n', '<leader>/', ':FzfLua lgrep_curbuf<cr>', opts)
 keymap('n', '<leader>sb', ':FzfLua buffers<cr>', opts)
 keymap('n', '<leader>sc', ':FzfLua git_commits<cr>', opts)
