24d332e1b79fa4d998d561649ec3af03095d0ae4
Eskild Hein Trøen <eht@equinor.com>
2023-03-21 08:46:33 +0100

    change vimwiki syntax to md

--- lua/setup/vimwiki.lua
+++ lua/setup/vimwiki.lua
@@ -1,3 +1,7 @@
+vim.cmd([[
+  let g:vimwiki_list = [{'syntax': 'markdown', 'ext': '.md', 'custom_wiki2html': 'vimwiki_markdown'}]
+]])
+
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 keymap('n', '<leader>i', ':VimwikiIndex<cr>:lcd %:h<cr>', opts)
