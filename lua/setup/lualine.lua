c8430f6afe9d03a6fb1cf108a17ac727b739145a
Eskild Hein Trøen <eht@equinor.com>
2023-03-30 08:14:20 +0200

    fix navic in lualine after update

--- lua/setup/lualine.lua
+++ lua/setup/lualine.lua
@@ -4,13 +4,13 @@ require('lualine').setup({
   winbar = {
     lualine_a = {'filesize'},
     lualine_b = {
-      { navic.get_location, cond = navic.is_available }
+      { function() return navic.get_location() end, cond = navic.is_available }
     }
   },
   inactive_winbar = {
     lualine_a = {'filesize'},
     lualine_b = {
-      { navic.get_location, cond = navic.is_available }
+      { function() return navic.get_location() end, cond = navic.is_available }
     }
   }
 })
