961dcc773d9aa5f59332feb2ea5f63dd1a2ae08f
Eskild Hein Trøen <eht@equinor.com>
2023-04-04 11:25:09 +0200

    use vimwiki default syntax

--- lua/setup/vimwiki.lua
+++ lua/setup/vimwiki.lua
@@ -1,7 +1,3 @@
-vim.cmd([[
-  let g:vimwiki_list = [{'syntax': 'markdown', 'ext': '.md', 'custom_wiki2html': 'vimwiki_markdown'}]
-]])
-
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 keymap('n', '<leader>i', ':VimwikiIndex<cr>:lcd %:h<cr>', opts)
