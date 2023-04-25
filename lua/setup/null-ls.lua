ae87fc727c50f60db62613111dd28c10839cba3a
Eskild Hein Trøen <37186215+eskildht@users.noreply.github.com>
2023-04-25 07:51:43 +0200

    null-ls disable format on save for now

--- lua/setup/null-ls.lua
+++ lua/setup/null-ls.lua
@@ -4,16 +4,17 @@ null_ls.setup({
     sources = {
         null_ls.builtins.formatting.prettierd,
     },
-    on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-              vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-              vim.api.nvim_create_autocmd("BufWritePre", {
-                  group = augroup,
-                  buffer = bufnr,
-                  callback = function()
-                      vim.lsp.buf.format({ bufnr = bufnr })
-                  end,
-              })
-          end
-      end,
+    -- uncomment below for format on save
+    -- on_attach = function(client, bufnr)
+    --       if client.supports_method("textDocument/formatting") then
+    --           vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --           vim.api.nvim_create_autocmd("BufWritePre", {
+    --               group = augroup,
+    --               buffer = bufnr,
+    --               callback = function()
+    --                   vim.lsp.buf.format({ bufnr = bufnr })
+    --               end,
+    --           })
+    --       end
+    --   end,
 })
