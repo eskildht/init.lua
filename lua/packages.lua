52abd5d9a885f351363e5d4efb267d8927f1ff12
Eskild Hein Trøen <37186215+eskildht@users.noreply.github.com>
2024-01-25 07:33:54 +0100

    harpoon mappings change and octo fix

--- lua/packages.lua
+++ lua/packages.lua
@@ -155,7 +155,7 @@ require('lazy').setup({
     lazy = true,
     cmd = 'Octo',
     config = function()
-      require('setup/octo')
+      require('octo').setup()
     end,
   },
   -- file creation, moving etc.
@@ -199,10 +199,10 @@ require('lazy').setup({
       vim.keymap.set("n", "<leader>h", function() harpoon:list():append() end)
       vim.keymap.set("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-      vim.keymap.set("n", "1", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "2", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "3", function() harpoon:list():select(3) end)
-      vim.keymap.set("n", "4", function() harpoon:list():select(4) end)
+      vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
     end
   }
 })
