eade4401e3b0ec9afaab95e07a270d7872e2706b
Eskild Hein Trøen <37186215+eskildht@users.noreply.github.com>
2024-01-25 07:34:12 +0100

    diffview conflict default diff4_mixed

--- lua/setup/diffview.lua
+++ lua/setup/diffview.lua
@@ -1,3 +1,10 @@
+require("diffview").setup({
+  view = {
+    merge_tool = {
+      layout = "diff4_mixed"
+    }
+  }
+})
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>do', ':DiffviewOpen<cr>', opts)
 vim.keymap.set('n', '<leader>dc', ':DiffviewClose<cr>', opts)
