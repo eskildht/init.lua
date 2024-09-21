d0849c4386213b1659ab34b7675ada594e4811ab
Eskild Hein Trøen <37186215+eskildht@users.noreply.github.com>
2024-09-21 21:50:02 +0200

    add cmp-nvim-lsp-signature-help

--- lua/packages.lua
+++ lua/packages.lua
@@ -49,6 +49,7 @@ require('lazy').setup({
   'neovim/nvim-lspconfig',
   -- completion engine
   'hrsh7th/cmp-nvim-lsp',
+  'hrsh7th/cmp-nvim-lsp-signature-help',
   'hrsh7th/cmp-buffer',
   'hrsh7th/cmp-path',
   'hrsh7th/cmp-cmdline',
--- lua/setup/cmp.lua
+++ lua/setup/cmp.lua
@@ -24,6 +24,7 @@ cmp.setup({
     {
       { name = 'nvim_lsp' },
       { name = 'snippy' },
+      { name = 'nvim_lsp_signature_help' }
     },
     {
       { name = 'buffer' }
