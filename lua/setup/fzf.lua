f8fdba0859532291d4097104d7eaa859b50c18ee
Eskild Hein Trøen <eskild.hein.troen@banenor.no>
2025-04-10 12:13:36 +0200

    fzf-lua git_commits mapping adjust and lsp maps

--- lua/setup/fzf.lua
+++ lua/setup/fzf.lua
@@ -7,7 +7,31 @@ require('fzf-lua').setup({
     fzf = {
       ['ctrl-q'] = 'select-all+accept'
     }
-  }
+  },
+  git = {
+    commits = {
+      fzf_opts = { ['--multi'] = true }, -- Enable multi-selection
+      actions = {
+        ['default'] = function(selected)
+          local commits = {}
+          for _, line in ipairs(selected) do
+            local hash = line:match('^%s*(%w+)') -- Match the commit hash at the start of the line
+            if hash then
+              table.insert(commits, hash)
+            end
+          end
+
+          if #commits == 1 then
+            vim.cmd('DiffviewOpen ' .. commits[1] .. "^!")
+          elseif #commits == 2 then
+            vim.cmd('DiffviewOpen ' .. commits[2] .. '..' .. commits[1])
+          else
+            vim.notify('Select one or two commits', vim.log.levels.WARN)
+          end
+        end,
+      },
+    },
+  },
 })
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
@@ -20,3 +44,5 @@ keymap('n', '<leader>sc', ':FzfLua git_commits<cr>', opts)
 keymap('n', '<leader>ss', ':FzfLua git_status<cr>', opts)
 keymap('n', '<leader>sk', ':FzfLua keymaps<cr>', opts)
 keymap('v', '<leader>sg', '<esc>:FzfLua grep_visual<cr>', opts)
+keymap('v', '<leader>sr', '<esc>:FzfLua lsp_references<cr>', opts)
+keymap('v', '<leader>si', '<esc>:FzfLua lsp_implementations<cr>', opts)
